a903c9082a4f6792cde8a2ec9f3d64bb9ba7ce06
0-00000 <461329610@qq.com>
2021-10-21 18:17:23 +0800

    Update lie.js

--- resource/js/lie.js
+++ resource/js/lie.js
@@ -40,13 +40,13 @@ video.onpause=function(){
 }
 video.onended=function(){
 	if(video.currentSrc=="https://github.com/0-00000/0-00000.github.io/releases/download/Big_datas/Never_Gonna_Give_You_Up-Rick_Astley.mp4"){
-		video.currentSrc="../resource/video/IndiHome Paket Phoenix.mp4";
+		video.src="../resource/video/IndiHome Paket Phoenix.mp4";
 		video.load();
 		video.currentTime=60;
 		w_h.width=1;
 		w_h.height=1;
 	}else{
-		video.currentSrc="https://github.com/0-00000/0-00000.github.io/releases/download/Big_datas/Never_Gonna_Give_You_Up-Rick_Astley.mp4";
+		video.src="https://github.com/0-00000/0-00000.github.io/releases/download/Big_datas/Never_Gonna_Give_You_Up-Rick_Astley.mp4";
 		video.load();
 		video.currentTime=0;
 		w_h.width=4;
